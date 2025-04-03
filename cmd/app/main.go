44cbd0849852ee4c9cc9ababdfe6a14da905c8f9
ajajmal <ajajmal@gmail.com>
2025-04-03 15:14:19 +0530

    added files for database setup

---
+++ cmd/app/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+"github.com/gin-gonic/gin"
+"log"
+"net/http"
+"github.com/ajajmal/my-go-app"
+)
+
+func main() {
+	r:=gin.Default();
+	r.GET("health", func(c *gin.Context){
+		c.JSON(http.statusOK, gin.H{"status":"OK"})
+	})
+
+	if err := r.Run(":8080"); err != nil {
+	log.Fatal(err)
+	}
+
+	config.ConnectDatabase()
+	config.DB.AutoMigrate(&models.User{})
+}
